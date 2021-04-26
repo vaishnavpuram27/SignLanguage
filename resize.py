f2fac71df22b0ac39a793d5eda58a219bd209fef
Vaishnav Puram <50790985+vaishnavpuram27@users.noreply.github.com>
2021-04-26 14:52:04 +0530

    edited this file

--- resize.py
+++ resize.py
@@ -1,10 +1,6 @@
 from PIL import Image
 
-def sizing(letter,lower_limit,upper_limit):
-    for i in range(lower_limit,upper_limit+1):
-        img = Image.open('train/'+letter+'/{0:03}.jpg'.format(i))
-        img = img.resize((200,150), Image.ANTIALIAS)
-        img.save('New/'+letter+'/{0:3}.jpg'.format(i-32))
-    print(letter + 'done..')
-
+img = Image.open('train/'+letter+'/{0:03}.jpg'.format(i))
+img = img.resize((200,150), Image.ANTIALIAS)
+img.save('New/'+letter+'/{0:3}.jpg'.format(i-32))
 
