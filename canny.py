f9b3b19acb97097cd858083fc2fb24820a1dbcb7
Vaishnav Puram <50790985+vaishnavpuram27@users.noreply.github.com>
2021-04-26 11:08:51 +0530

    these were modifed

--- canny.py
+++ canny.py
@@ -2,14 +2,15 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-myList = ['A','B','C','D','del','E','F','G','H','I','J','K','L','M','N','nothing','O','P','Q','R','S','space','T','U','V','W','X','Y','Z']
+myList1 = ['A','B','C','D','del','E','F','G','H','I','J','K','L','M','N','nothing','O','P','Q','R','S','space','T','U','V','W','X','Y','Z']
+myList=[['A',242],['B',258],['C',247],['D',147],['E',243],['F',226],['G',241],['H',116],['I',179],['K',245],['L',182],['M',235],['N',237],['O',229],['P',234],['Q',216],['R',210],['S',229],['T',216],['U',135],['V',122],['W',128],['X',201],['Y',251]]
 
 for j in range(len(myList)):
-    for i in range(1,3001):
-        img = cv2.imread('output2/'+str(myList[j])+'/G'+str(myList[j])+str(i)+'.png',0) 
+    for i in range(1,myList[j][1]+1):
+        img = cv2.imread('output5/'+str(myList[j][0])+'/G'+str(myList[j][0])+str(i)+'.jpg',0) 
         canny = cv2.Canny(img ,100,200)
-        cv2.imwrite('output3/'+str(myList[j])+'/C'+str(myList[j])+str(i)+'.png',canny)
-    print(str(myList[j])+ ' done')
+        cv2.imwrite('output6/'+str(myList[j][0])+'/C'+str(myList[j][0])+str(i)+'.jpg',canny)
+    print(str(myList[j][0])+ ' done')
 
 
 
--- gou.py
+++ gou.py
@@ -2,13 +2,15 @@
 import numpy as np
 import cv2
 
-myList = ['A','B','C','D','del','E','F','G','H','I','J','K','L','M','N','nothing','O','P','Q','R','S','space','T','U','V','W','X','Y','Z']
+def grey(letter,upper_limt):
+    for i in range(1,upper_limt+1):
+        img = cv2.imread('New/'+letter+'/'+letter+str(i)+'.jpg',0) 
+        # grey= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite('output5/'+letter+'/G'+str(letter)+str(i)+'.jpg',img)
+    print(letter+ ' done')
 
-for j in range(len(myList)):
-    for i in range(1,3001):
-        img = cv2.imread('input/'+str(myList[j])+'/'+str(myList[j])+str(i)+'.jpg') 
-        grey= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('output2/'+str(myList[j])+'/G'+str(myList[j])+str(i)+'.png',grey)
-    print(str(myList[j])+ ' done')
+myList=[['A',242],['B',258],['C',247],['D',147],['E',243],['F',226],['G',241],['H',116],['I',179],['K',245],['L',182],['M',235],['N',237],['O',229],['P',234],['Q',216],['R',210],['S',229],['T',216],['U',135],['V',122],['W',128],['X',201],['Y',251]]
+
+
 
 
